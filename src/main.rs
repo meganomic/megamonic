71f1bc095e5ecd7fcd8554d37a899ea7718af159
meganomic <>
2021-02-25 13:11:16 +0100

    Add message in --help for quitting. Forgot about hat

--- src/main.rs
+++ src/main.rs
@@ -102,7 +102,7 @@ fn main() -> Result<()> {
                 .help("Update frequency in milliseconds. Min: 1000, Max: 5000")
                 .default_value("1000")
         )
-        .after_help("\x1b[91mEnabling both smaps and all processes is ultra slow.\nEspecially if running as root.\x1b[0m\n\nYou can toggle some things by pressing these buttons:\nPress 'a' to toggle all processes.\nPress 's' to toggle smaps.\nPress 't' to toggle \"Top mode\"\nPress 'r' to rebuild the UI incase it's broken\nPress [space] to pause the UI.")
+        .after_help("\x1b[91mEnabling both smaps and all processes is ultra slow.\nEspecially if running as root.\x1b[0m\n\nYou can toggle some things by pressing these buttons:\nPress 'q' to exit.\nPress 'a' to toggle all processes.\nPress 's' to toggle smaps.\nPress 't' to toggle \"Top mode\"\nPress 'r' to rebuild the UI incase it's broken\nPress [space] to pause the UI.")
         .get_matches();
 
     let stdout_l = stdout();
