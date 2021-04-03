2406b5e9d1eaf1629619a3822f7209878a5f952e
meganomic <>
2021-04-03 15:28:47 +0200

    Minor refactor

--- src/system/sensors.rs
+++ src/system/sensors.rs
@@ -18,21 +18,19 @@ impl Sensors {
     pub fn update(&mut self) {
         // Don't handle any errors, just skip that sensor.
         for chip in self.sensors {
-            if let Ok(c_name) = chip.get_name() {
+            if let Ok(mut c_name) = chip.get_name() {
                 for feature in chip {
                     if let Ok(f_name) = feature.get_label() {
-                        for subfeature in feature {
+                        if let Some(subfeature) = feature.get_subfeature(sensors::SubfeatureType::SENSORS_SUBFEATURE_TEMP_INPUT) {
                             if let Ok(val) = subfeature.get_value() {
-                                // We only want tempX_input
-                                if subfeature.name().ends_with("input") {
-                                    // If the name is temp1 that means it doesn't have a f_name
-                                    // So use the chip name instead
-                                    if f_name == "temp1" {
-                                        self.chips.insert(c_name.clone(), val.round() as u8);
-                                    }
-                                    else {
-                                        self.chips.insert(f_name.clone(), val.round() as u8);
-                                    }
+                                // If the name is temp1 that means it doesn't have a f_name
+                                // So use the chip name instead
+                                if f_name == "temp1" {
+                                    c_name.truncate(14);
+                                    self.chips.insert(c_name.clone(), val.round() as u8);
+                                }
+                                else {
+                                    self.chips.insert(f_name, val.round() as u8);
                                 }
                             }
                         }
@@ -44,37 +42,37 @@ impl Sensors {
 }
 
 pub fn start_thread(internal: Arc<Mutex<Sensors>>, tx: mpsc::Sender::<u8>, exit: Arc<(std::sync::Mutex<bool>, std::sync::Condvar)>, sleepy: std::time::Duration) -> std::thread::JoinHandle<()> {
-    std::thread::spawn(move || 'outer: loop {
-        match internal.lock() {
-            Ok(mut val) => {
-                val.update();
-            },
-            Err(_) => break,
-        };
-        match tx.send(6) {
-            Ok(_) => (),
-            Err(_) => break,
-        };
-
+    std::thread::spawn(move || {
         let (lock, cvar) = &*exit;
-        if let Ok(mut exitvar) = lock.lock() {
-            loop {
-                if let Ok(result) = cvar.wait_timeout(exitvar, sleepy) {
-                    exitvar = result.0;
+        'outer: loop {
+            match internal.lock() {
+                Ok(mut val) => val.update(),
+                Err(_) => break,
+            };
+            match tx.send(6) {
+                Ok(_) => (),
+                Err(_) => break,
+            };
 
-                    if *exitvar == true {
+            if let Ok(mut exitvar) = lock.lock() {
+                loop {
+                    if let Ok(result) = cvar.wait_timeout(exitvar, sleepy) {
+                        exitvar = result.0;
+
+                        if *exitvar == true {
+                            break 'outer;
+                        }
+
+                        if result.1.timed_out() == true {
+                            break;
+                        }
+                    } else {
                         break 'outer;
                     }
-
-                    if result.1.timed_out() == true {
-                        break;
-                    }
-                } else {
-                    break 'outer;
                 }
+            } else {
+                break;
             }
-        } else {
-            break;
         }
     })
 }
