1eda126ab6e6250eff0070f2cf63d453284851b9
meganomic <>
2021-04-03 10:25:22 +0200

    Use tuple instead of vector for cache

--- src/ui/swap.rs
+++ src/ui/swap.rs
@@ -11,7 +11,7 @@ pub struct Swap <'a> {
     pub pos: XY,
     pub size: XY,
 
-    cache: Vec::<String>,
+    cache: (String, String, String),
 
     total: i64,
     free: i64,
@@ -22,14 +22,9 @@ pub struct Swap <'a> {
 
 impl <'a> Swap <'a> {
     pub fn new(system: &'a System, pos: XY) -> Self {
-        let mut cache = Vec::<String>::new();
-        cache.push(String::new());
-        cache.push(String::new());
-        cache.push(String::new());
-
         Self {
             system,
-            cache,
+            cache: (String::new(), String::new(), String::new()),
             total: 0,
             free: 0,
             total_str: String::new(),
@@ -41,57 +36,50 @@ impl <'a> Swap <'a> {
     }
 
     pub fn rebuild_cache (&mut self) {
-        unsafe {
-            let cache1 = self.cache.get_unchecked_mut(0);
-            cache1.clear();
-            cache1.push_str(&format!(
-                "{}\x1b[95mSwap\x1b[0m{}                  {}\x1b[37mTotal: \x1b[38;5;244m[ \x1b[37m",
-                cursor::MoveTo(self.pos.x, self.pos.y),
-                cursor::MoveTo(self.pos.x, self.pos.y+1),
-                cursor::MoveTo(self.pos.x, self.pos.y+1)
-            ));
+        self.cache.0.clear();
+        self.cache.0.push_str(&format!(
+            "{}\x1b[95mSwap\x1b[0m{}                  {}\x1b[37mTotal: \x1b[38;5;244m[ \x1b[37m",
+            cursor::MoveTo(self.pos.x, self.pos.y),
+            cursor::MoveTo(self.pos.x, self.pos.y+1),
+            cursor::MoveTo(self.pos.x, self.pos.y+1)
+        ));
 
-            let cache2 = self.cache.get_unchecked_mut(1);
-            cache2.clear();
-            cache2.push_str(&format!(
-                "\x1b[38;5;244m ]\x1b[0m{}                  {}\x1b[37mUsed:  \x1b[91m[ \x1b[92m",
-                cursor::MoveTo(self.pos.x, self.pos.y+2),
-                cursor::MoveTo(self.pos.x, self.pos.y+2)
-            ));
+        self.cache.1.clear();
+        self.cache.1.push_str(&format!(
+            "\x1b[38;5;244m ]\x1b[0m{}                  {}\x1b[37mUsed:  \x1b[91m[ \x1b[92m",
+            cursor::MoveTo(self.pos.x, self.pos.y+2),
+            cursor::MoveTo(self.pos.x, self.pos.y+2)
+        ));
 
-            let cache3 = self.cache.get_unchecked_mut(2);
-            cache3.clear();
-            cache3.push_str(&format!(
-                "\x1b[91m ]\x1b[0m{}                  {}\x1b[37mFree:  \x1b[38;5;244m[ \x1b[37m",
-                cursor::MoveTo(self.pos.x, self.pos.y+3),
-                cursor::MoveTo(self.pos.x, self.pos.y+3)
-            ));
-        }
+        self.cache.2.clear();
+        self.cache.2.push_str(&format!(
+            "\x1b[91m ]\x1b[0m{}                  {}\x1b[37mFree:  \x1b[38;5;244m[ \x1b[37m",
+            cursor::MoveTo(self.pos.x, self.pos.y+3),
+            cursor::MoveTo(self.pos.x, self.pos.y+3)
+        ));
     }
 
     pub fn draw (&mut self, stdout: &mut std::io::Stdout) -> Result<()> {
         if let Ok(val) = self.system.swapinfo.lock() {
-            unsafe {
-                if self.total != val.total {
-                    self.total = val.total;
-                    self.total_str = convert_with_padding(val.total, 4);
-                }
-
-                if self.free != val.free {
-                    self.free = val.free;
-                    self.free_str = convert_with_padding(val.free, 4);
-                    self.used_str = convert_with_padding(val.used, 4);
-                }
-
-                write!(stdout, "{}{}{}{}{}{}\x1b[38;5;244m ]\x1b[0m",
-                    &self.cache.get_unchecked(0),
-                    &self.total_str,
-                    &self.cache.get_unchecked(1),
-                    &self.used_str,
-                    &self.cache.get_unchecked(2),
-                    &self.free_str
-                )?;
+            if self.total != val.total {
+                self.total = val.total;
+                self.total_str = convert_with_padding(val.total, 4);
             }
+
+            if self.free != val.free {
+                self.free = val.free;
+                self.free_str = convert_with_padding(val.free, 4);
+                self.used_str = convert_with_padding(val.used, 4);
+            }
+
+            write!(stdout, "{}{}{}{}{}{}\x1b[38;5;244m ]\x1b[0m",
+                &self.cache.0,
+                &self.total_str,
+                &self.cache.1,
+                &self.used_str,
+                &self.cache.2,
+                &self.free_str
+            )?;
         }
 
         Ok(())
