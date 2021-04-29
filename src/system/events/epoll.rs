6971ef47e29f1674092db255fb43fa1cefa71706
meganomic <10358690+meganomic@users.noreply.github.com>
2021-04-29 11:18:54 +0200

    Using custom sigaddset, portability is for casuals

--- src/system/events/epoll.rs
+++ src/system/events/epoll.rs
@@ -46,29 +46,7 @@ const EXTPROC: u32 = 0200000;
 const B4000000: u32 = 0010017;
 
 #[repr(C)]
-#[derive(Debug)]
-struct Termios {
-    c_iflag: u32,           /* input mode flags */
-    c_oflag: u32,            /* output mode flags */
-    c_cflag: u32,            /* control mode flags */
-    c_lflag: u32,           /* local mode flags */
-    c_line: u8,                        /* line discipline */
-    c_cc: [u8; NCSS],            /* control characters */
-    c_ispeed: u32,           /* input speed */
-    c_ospeed: u32           /* output speed */
-}
-
-#[repr(C)]
-#[derive(Debug,Default)]
-struct Winsize {
-    ws_row: u16,
-    ws_col: u16,
-    ws_xpixel: u16,   /* unused */
-    ws_ypixel: u16,   /* unused */
-}
-
-#[repr(C)]
-#[derive(Default, Debug)]
+#[derive(Default)]
 pub struct SignalfdSiginfo {
     pub ssi_signo: u32,    /* Signal number */
     ssi_errno: i32,    /* Error number (unused) */
@@ -98,15 +76,7 @@ pub struct SignalfdSiginfo {
                         additional fields in the future) */
 }
 
-pub struct SignalFD {
-    pub fd: i32,
-}
-
-/*#[repr(C)]
-pub struct sigset_t {
-    __val: [u64; 16],
-}*/
-
+// Custom sigaddset(), portability is for casuals
 fn sigmask(sig: i32) -> u64 {
     1u64 << ((sig - 1) % 64)
 }
@@ -115,89 +85,49 @@ fn sigword(sig: i32) -> u64 {
     ((sig - 1) / 64) as u64
 }
 
-fn sigaddset(set: &mut [u64; 16], sig: i32) {
+fn sigaddset(set: &mut [u64; 8], sig: i32) {
     let mask = sigmask(sig);
     let word = sigword(sig);
 
-    eprintln!("mas: {} word: {}", mask, word);
-
     set[word as usize] |= mask;
-
 }
 
-// [134217728, 140019846132652, 206158430211, 0, 2, 7, 64, 56, 4, 18, 96, 1, 210453397508, 0, 0, 0]
+pub struct SignalFD {
+    pub fd: i32,
+}
 
 impl SignalFD {
     pub fn new() -> Self {
-        // Emptry sigset_t struct
-        //let mut sigset: libc::sigset_t = unsafe { std::mem::uninitialized()  };
-        let mut sigset: libc::sigset_t = unsafe { std::mem::MaybeUninit::zeroed().assume_init() };
+        // Custom verison
+        let mut set: [u64; 8] = [0; 8];
 
-        /*let mut set: [u8; 128] = [1; 128];
-        eprintln!("set: {:?}", set);
-        let mut set2 = unsafe { std::mem::transmute::<[u8; 128], libc::sigset_t>(set) };
+        sigaddset(&mut set, 28); // SIGWINCH
+        sigaddset(&mut set, 2); // SIGINT
 
-        unsafe { libc::sigemptyset(&mut set2) };
-        let set3 = unsafe { std::mem::transmute::<libc::sigset_t, [u8; 128]>(set2) };
-        eprintln!("set3: {:?}", set3);*/
-
-
-        //let mut set: [u64; 16] = [0; 16];
-        //let shoe = set[0];
-        //sigaddset(&mut set, libc::SIGWINCH);
-        //eprintln!("custom: {:?}", set);
-
-
-
-        /*let bajs: [u64; 16] = unsafe { std::mem::transmute_copy(&sigset) };
-        eprintln!("siget1: {:?}", bajs);*/
-        // Initialize sigset_t
-        unsafe { libc::sigemptyset(&mut sigset) };
-        /*let bajs: [u64; 16] = unsafe { std::mem::transmute_copy(&sigset) };
-        eprintln!("siget2: {:?}", bajs);*/
-
-        // Set SIGWINCH to be handled by us
-        unsafe { libc::sigaddset(&mut sigset, libc::SIGWINCH) };
-        unsafe { libc::sigaddset(&mut sigset, libc::SIGINT) };
-
-        /*let bajs: [u64; 16] = unsafe { std::mem::transmute_copy(&sigset) };
-        eprintln!("siget3: {:?}", bajs);*/
-        //libc::sigaddset(sigset, 28);
-
-
-        // Disable default handling of SIGWINCH
-        unsafe { libc::pthread_sigmask(libc::SIG_BLOCK, &sigset, 0 as *mut libc::sigset_t) };
-
-        let fd = unsafe { libc::signalfd(-1, &sigset, 0) };
-
-        /*let ret: i32;
+        let ret: i32;
         unsafe {
             asm!("syscall",
                 in("rax") 14, // SYS_SIGNALFD4
                 in("rdi") 2, // SIG_SETMASK
                 in("rsi") set.as_ptr(), //&sigset as *const libc::sigset_t, // 	sigset_t __user * nset
                 in("rdx") 0, // sigset_t __user * oset
-                in("r10") 8, // size_t sigsetsize
+                in("r10") set.len(), // size_t sigsetsize
                 out("rcx") _,
                 out("r11") _,
                 lateout("rax") ret,
             );
         }
 
-        assert!(ret == 0);*/
-
-        //eprintln!("ret: {}", ret);
-
-        //let fd = unsafe { libc::signalfd(-1, &sigset, 0) };
+        assert!(ret == 0);
 
         // Make a signalfd file
-        /*let fd: i32;
+        let fd: i32;
         unsafe {
             asm!("syscall",
                 in("rax") 289, // SYS_SIGNALFD4
                 in("rdi") -1, // -1 == create a new signalfd
                 in("rsi") set.as_ptr(), //&sigset as *const libc::sigset_t, // user_mask
-                in("rdx") 8, // sizemask = u64 == 8, u32 == 4
+                in("rdx") set.len(), // sizemask = u64 == 8, u32 == 4
                 in("r10") 0, // flags
                 out("rcx") _,
                 out("r11") _,
@@ -205,9 +135,25 @@ impl SignalFD {
             );
         }
 
-        //println!("ret: {}", fd);
+        assert!(!fd.is_negative());
 
-        assert!(!fd.is_negative());*/
+        /* libc version
+        let mut sigset: libc::sigset_t = unsafe { std::mem::MaybeUninit::zeroed().assume_init() };
+
+        // Initialize sigset_t
+        unsafe { libc::sigemptyset(&mut sigset) };
+
+        // Set SIGWINCH to be handled by us
+        unsafe { libc::sigaddset(&mut sigset, libc::SIGWINCH) };
+        unsafe { libc::sigaddset(&mut sigset, libc::SIGINT) };
+
+
+        // Disable default handling of SIGWINCH
+        unsafe { libc::pthread_sigmask(libc::SIG_BLOCK, &sigset, 0 as *mut libc::sigset_t) };
+
+
+        let fd = unsafe { libc::signalfd(-1, &sigset, 0) };
+        */
 
         Self {
             fd
