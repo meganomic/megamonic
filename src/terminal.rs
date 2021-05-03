f07651ac4a9e87bf55169153424675cf345d6f4a
meganomic <10358690+meganomic@users.noreply.github.com>
2021-05-03 08:59:59 +0200

    Minor code improvements

--- src/terminal.rs
+++ src/terminal.rs
@@ -30,8 +30,19 @@ const CSIZE: u32 =  0o60;
 const CS8: u32 =    0o60;
 const PARENB: u32 = 0o400;
 
+static mut TTYTERMIOS: Termios = Termios {
+            c_iflag: 0,
+            c_oflag: 0,
+            c_cflag: 0,
+            c_lflag: 0,
+            c_line: 0,
+            c_cc: [0; NCSS],
+        };
+
+static mut TTYFD: i32 = 0;
+
+
 #[repr(C)]
-#[derive(Debug)]
 struct Termios {
     c_iflag: u32,           /* input mode flags */
     c_oflag: u32,            /* output mode flags */
@@ -50,23 +61,12 @@ struct Winsize {
     ws_ypixel: u16,   /* unused */
 }
 
-// This global stuff is needed for the custom_panic_hook()
-static mut TTYTERMIOS: Termios = Termios {
-            c_iflag: 0,
-            c_oflag: 0,
-            c_cflag: 0,
-            c_lflag: 0,
-            c_line: 0,
-            c_cc: [0; NCSS],
-        };
-
-static mut TTYFD: i32 = 0;
-
 // Make sure statics are initialized
-#[inline(always)]
-fn check_statics() {
-    if unsafe { TTYFD == 0 } {
-        init();
+macro_rules! check_statics {
+    () => {
+        if unsafe { TTYFD == 0 } {
+            init();
+        }
     }
 }
 
@@ -88,23 +88,19 @@ fn init() {
 
     assert!(!fd.is_negative());
 
-    let termios = Termios {
-        c_iflag: 0,
-        c_oflag: 0,
-        c_cflag: 0,
-        c_lflag: 0,
-        c_line: 0,
-        c_cc: [0; NCSS],
-    };
+    // Save tty fd
+    unsafe {
+        TTYFD = fd;
+    }
 
-    // Save original tty settings
+    // Save original tty settings to TTYTERMIOS
     let ret: i32;
     unsafe {
         asm!("syscall",
             in("rax") 16, // SYS_IOCTL
             in("rdi") fd,
             in("rsi") TCGETS,
-            in("rdx") &termios as *const Termios,
+            in("rdx") &TTYTERMIOS as *const Termios,
             out("rcx") _,
             out("r11") _,
             lateout("rax") ret,
@@ -112,28 +108,24 @@ fn init() {
     }
 
     assert!(!ret.is_negative());
-
-    // Need these statics for the custom panic hook
-    unsafe {
-        TTYTERMIOS = termios;
-        TTYFD = fd;
-    }
 }
 
 // Enable raw mode
 pub fn enable_raw_mode() {
-    check_statics();
+    check_statics!();
 
     // Enable raw mode settings, but still send signals. c_flag | ISIG
-    let termios = unsafe { Termios {
-        c_iflag: TTYTERMIOS.c_iflag & !(IGNBRK | BRKINT | PARMRK | ISTRIP
-                | INLCR | IGNCR | ICRNL | IXON),
-        c_oflag: TTYTERMIOS.c_oflag & !OPOST,
-        c_lflag: TTYTERMIOS.c_lflag & !(ECHO | ECHONL | ICANON | IEXTEN),
-        c_cflag: TTYTERMIOS.c_cflag & !(CSIZE | PARENB) | CS8,
-        c_line: TTYTERMIOS.c_line,
-        c_cc: TTYTERMIOS.c_cc,
-    } };
+    let termios = unsafe {
+        Termios {
+            c_iflag: TTYTERMIOS.c_iflag & !(IGNBRK | BRKINT | PARMRK | ISTRIP
+                    | INLCR | IGNCR | ICRNL | IXON),
+            c_oflag: TTYTERMIOS.c_oflag & !OPOST,
+            c_lflag: TTYTERMIOS.c_lflag & !(ECHO | ECHONL | ICANON | IEXTEN),
+            c_cflag: TTYTERMIOS.c_cflag & !(CSIZE | PARENB) | CS8,
+            c_line: TTYTERMIOS.c_line,
+            c_cc: TTYTERMIOS.c_cc,
+        }
+    };
 
     // Set tty with our new settings
     let ret: i32;
@@ -141,7 +133,7 @@ pub fn enable_raw_mode() {
         asm!("syscall",
             in("rax") 16, // SYS_IOCTL
             in("rdi") TTYFD,
-            in("rsi") TCSETS, // O_RDONLY
+            in("rsi") TCSETS,
             in("rdx") &termios as *const Termios,
             out("rcx") _,
             out("r11") _,
@@ -154,14 +146,15 @@ pub fn enable_raw_mode() {
 
 // Reset tty settings to original settings and close tty fd
 pub fn disable_raw_mode() {
-    check_statics();
+    check_statics!();
 
+    // Set tty settings to our saved original values
     let ret: i32;
     unsafe {
         asm!("syscall",
             in("rax") 16, // SYS_IOCTL
             in("rdi") TTYFD,
-            in("rsi") TCSETS, // O_RDONLY
+            in("rsi") TCSETS,
             in("rdx") &TTYTERMIOS as *const Termios,
             out("rcx") _,
             out("r11") _,
@@ -171,13 +164,12 @@ pub fn disable_raw_mode() {
 
     assert!(!ret.is_negative());
 
+    // Close the tty fd
     let ret: i32;
     unsafe {
         asm!("syscall",
             in("rax") 3, // SYS_CLOSE
             in("rdi") TTYFD,
-            //in("rsi") 0, // O_RDONLY
-            //in("rdx") 0, // This is the mode. It is not used in this case
             out("rcx") _,
             out("r11") _,
             lateout("rax") ret,
@@ -187,16 +179,16 @@ pub fn disable_raw_mode() {
     assert!(!ret.is_negative());
 }
 
-// Send char to terminal input stream
+// Send char to terminal input stream, used to exit the Event thread
 pub fn send_char(c: &str) {
-    check_statics();
+    check_statics!();
 
     let ret: i32;
     unsafe {
         asm!("syscall",
             in("rax") 16, // SYS_IOCTL
             in("rdi") TTYFD,
-            in("rsi") TIOCSTI, // O_RDONLY
+            in("rsi") TIOCSTI,
             in("rdx") c.as_ptr(),
             out("rcx") _,
             out("r11") _,
@@ -209,7 +201,7 @@ pub fn send_char(c: &str) {
 
 // Get the size of the terminal
 pub fn gettermsize() -> (u16, u16) {
-    check_statics();
+    check_statics!();
 
     let mut winsize = Winsize::default();
 
@@ -218,7 +210,7 @@ pub fn gettermsize() -> (u16, u16) {
         asm!("syscall",
             in("rax") 16, // SYS_IOCTL
             in("rdi") TTYFD,
-            in("rsi") TIOCGWINSZ, // O_RDONLY
+            in("rsi") TIOCGWINSZ,
             in("rdx") &mut winsize as *mut Winsize,
             out("rcx") _,
             out("r11") _,
