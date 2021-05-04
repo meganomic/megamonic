5a140c2cfe08526064931692f3140d0e5651db08
meganomic <10358690+meganomic@users.noreply.github.com>
2021-05-04 10:23:57 +0200

    Minor code improvements

--- src/system/hostinfo.rs
+++ src/system/hostinfo.rs
@@ -6,30 +6,31 @@ pub struct Hostinfo {
 
 impl Default for Hostinfo {
     fn default() -> Self {
-        let mut distname = String::new();
+        let mut distname = String::from("Not Found");
         let mut ansi_color = String::from("\x1b[97m");
 
-        let osrelease = std::fs::read_to_string("/etc/os-release").unwrap_or_else(|_| "NAME=\"Not Found\"".to_string());
+        if let Ok(osrelease) = std::fs::read_to_string("/etc/os-release") {
+            for line in osrelease.lines() {
+                if line.starts_with("NAME") {
+                    if let Some(pos) = line.find('"') {
+                        distname.clear();
+                        distname.push_str(&line[pos+1..line.len()-1].trim());
+                    }
+                } else if line.starts_with("ANSI_COLOR") {
+                    if let Some(pos) = line.find('"') {
+                        ansi_color.clear();
+                        let color = &line[pos+1..line.len()-1];
+                        ansi_color.push_str("\x1b[");
+                        ansi_color.push_str(color);
+                        ansi_color.push('m');
 
-        for line in osrelease.lines() {
-            if line.starts_with("NAME") {
-                if let Some(pos) = line.find('"') {
-                    distname.push_str(&line[pos+1..line.len()-1].trim());
-                }
-            } else if line.starts_with("ANSI_COLOR") {
-                if let Some(pos) = line.find('"') {
-                    ansi_color.clear();
-                    let color = &line[pos+1..line.len()-1]; //.unwrap_or("\x1b[97m").to_string();
-                    ansi_color.push_str("\x1b[");
-                    ansi_color.push_str(color);
-                    ansi_color.push('m');
-
+                    }
                 }
             }
         }
 
         let kernel = std::fs::read_to_string("/proc/sys/kernel/osrelease")
-            .unwrap_or_else(|_|"Not Found".to_string())
+            .unwrap_or_else(|_| "Not Found".to_string())
             .trim()
             .to_string();
 
